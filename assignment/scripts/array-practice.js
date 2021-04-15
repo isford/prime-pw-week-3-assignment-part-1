26f7cd9355fd2e200acb5c58af9f1c0004d456e3
Ian <isford7@gmail.com>
2021-04-15 10:41:27 -0500

    Done with part 1, not including stretch goals

--- assignment/scripts/array-practice.js
+++ assignment/scripts/array-practice.js
@@ -31,10 +31,10 @@ console.log('--- 3. Accessing items in an array ---');
 console.log('First animal is', animalArray[0])
 
 // 3.a. TODO: Log the second animal in the array
-
+console.log("Second animal is", animalArray[1]);
 
 // 3.b. TODO: Log the last animal in the array using it's array index
-
+console.log('The last animal is',animalArray[3]);
 
 // 3.c. (STRETCH) TODO: Log the last animal by using the array length,
 //      instead of the exact index number of the last item
@@ -49,8 +49,8 @@ animalArray.push('penguin');
 console.log('Added an animal to end,', animalArray);
 
 // 4.a. TODO: Add a new food at the end of your array & log the array
-
-
+foodArray.push("Green Leafy Things")
+console.log('Added a new food to end,', foodArray);
 
 // Example: Remove the last animal by using Array.pop
 let removedAnimal = animalArray.pop();
@@ -59,14 +59,17 @@ console.log('The animals are now', animalArray);
 
 // 4.b. TODO: Remove the food at the end of your array &
 //      log both the food removed and the updated array
-
+let removedFood = foodArray.pop();
+console.log('Removed the last food', removedFood);
+console.log('The foods are now', foodArray);
 
 // Example: Add an animal to the beginning using Array.unshift
 animalArray.unshift('walrus');
 console.log(`Added an animal to beginning: ${animalArray}`);
 
 // 4.c. TODO: Add a food at the beginning of the array & log the array
-
+foodArray.unshift('apple');
+console.log('Added a food to beginning', foodArray);
 
 // Example: Remove the first animal using Array.shift
 removedAnimal = animalArray.shift();
@@ -75,3 +78,6 @@ console.log('The animals are now', animalArray);
 
 // 4.d TODO: Remove the food at the beginning of your array &
 //     log both the food removed and the updated array
+removedFood = foodArray.shift();
+console.log('Removed the first food,', removedFood);
+console.log('The food is now', foodArray);
